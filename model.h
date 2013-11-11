4758362dd9bcb8adc920fe8c2af11e51cc68d305
Yaping Lang <yl2025@bath.ac.uk>
2013-11-11 12:43:03 +0000

    Added the model.h file.

---
+++ model.h
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  cw2
+//
+//  Created by Lang Yaping on 11/11/2013.
+//  Copyright (c) 2013 Lang Yaping. All rights reserved.
+//
+
+#include <iostream>
+
+class model{
+    map<String,Passenger*> m_passengers;
+    map<String,Flights*> m_flgihtsbyCode;
+    map<String,Flights*> m_flightByDare;
+    model* m_model;
+    
+}
