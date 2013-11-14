4d54f0ac10da8ea18d9b9f1d2b4a9656a735cf37
aciucanu <aciucanu@localhost>
2013-11-14 08:22:52 +0000

    Update on Controller header

--- Controller.h
+++ Controller.h
@@ -10,21 +10,26 @@ class Controller
 //The controller class that stands between the data (model) and the visual display (view)
 
 private:
+//Pointers to the model and the view
     Model* m_model;
     View* m_view;
 
 public:
+
+//Constructor and destructor of Xontroller class
     Controller(Model* _model, View *_view);
     ~Controller();
 
+//Methods for accessing the model
     string makeReservation(string _name, string _number, SeatClass _class);
     string makeCancellation(string _name, string _code);
     vector<Flight*> makePassengerEnquiry(string _name);
     vector <Passenger*> makeFlightEnquiry(string _code, SeatClass _class);
     vector <Passenger*> makeFlightWaitingEnquiry(string _code, SeatClass _class);
 
-
-
+//Methods for accessing the view
+	void displayPassengers(vector<Passenger*>* _passengers);	
+	void displayFlights(vector<Flight*>* _flights);	
 };
 
 #endif // CONTROLLER_H
