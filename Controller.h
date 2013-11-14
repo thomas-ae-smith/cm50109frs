d0205ad817728c5328286680f3e40e023143ed79
aciucanu <aciucanu@localhost>
2013-11-14 08:01:18 +0000

    Update on Controller header

--- Controller.h
+++ Controller.h
@@ -5,18 +5,20 @@
 #include "Flight.h"
 
 
-class Crontroller
+class Controller
 {
+//The controller class that stands between the data (model) and the visual display (view)
+
 private:
     Model* m_model;
     View* m_view;
 
 public:
-    Controller(string _filename);
+    Controller(Model* _model, View *_view);
     ~Controller();
 
     string makeReservation(string _name, string _number, SeatClass _class);
-    string makeCancellation(string _name, string _number);
+    string makeCancellation(string _name, string _code);
     vector<Flight*> makePassengerEnquiry(string _name);
     vector <Passenger*> makeFlightEnquiry(string _code, SeatClass _class);
     vector <Passenger*> makeFlightWaitingEnquiry(string _code, SeatClass _class);
