0ea8e8b289e6a419717a59335b129f32ab694a2a
taes1g09 <thomas.ae.smith@googlemail.com>
2013-11-18 10:27:23 +0000

    Added comments to the Flight.cpp file.

--- Flight.cpp
+++ Flight.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+
+// create a flight with input parameters
 Flight::Flight(string _number, string _time, string _date, int _seatTotal) {
 	this.m_number = _number;
 	this.m_time = _time;
@@ -16,24 +18,31 @@ Flight::Flight(string _number, string _time, string _date, int _seatTotal) {
 	this.m_seatlist[SeatClass.Economy] = new vector<passenger*>();
 }
 
+// free the memory allocated for the new vectors
 Flight::~Flight() {
-	this.m_seatlist[SeatClass.First] = new vector<passenger*>();
-	this.m_seatlist[SeatClass.Economy] = new vector<passenger*>();
+	delete this.m_seatlist[SeatClass.First];
+	delete this.m_seatlist[SeatClass.Economy];
 }
 
+// put a passenger into the queue for the appropriate flight
 void Flight::addPassenger(Passenger* _passenger, SeatClass _class) {
 	m_seatlist[_class].push_back(_passenger);
 }
 
+// remove a passenger if they exist in the waiting lists
 void Flight::removePassenger(Passenger* _passenger) {
+	vector<Passenger*> *first = &m_seatlist[SeatClass.First];
+	vector<Passenger*> *economy = &m_seatlist[SeatClass.Economy];
 	int index = -1;
 
-	if ((index = find(first->begin(), first->end(), _passenger)) >0 )
-	{
-		/* code */
+	if ((index = find(first->begin(), first->end(), _passenger)) >0 ) {
+		first.erase(index)
+	} else if ((index = find(economy->begin(), economy->end(), _passenger)) >0 ) {
+		economy.erase(index)
 	}
 }
 
+// find if a passenger exists on the passenger lists
 bool Flight::passengerOnFlight(Passenger* _passenger) {
 	vector<Passenger*> *first = &m_seatlist[SeatClass.First];
 	vector<Passenger*> *economy = &m_seatlist[SeatClass.Economy];
