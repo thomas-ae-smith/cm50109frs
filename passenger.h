e5c62e4200c9ed41abcaaacfef1caeb81a9a47ef
ehuffman <eeh26@bath.ac.uk>
2013-11-18 10:30:06 +0000

    Updated diary, added comments to passenger.h, updated passenger.cpp

--- passenger.h
+++ passenger.h
@@ -4,20 +4,33 @@
 #include "Flight.h"
 
 
-class Passenger{
-
+class Passenger
+{
 private:
 //Data
+
+//The name of the person.
 string m_name;
+//The list of flights the person is on or waiting for a place on.
+//can we change this so it has flight pointers and waiting/not? or change getFlights?
 vector<Flight*> m_flights;
 
 public:
 //Methods
+
+//constructor
 Passenger (string _name);
+//destructor
 ~Passenger();
+//sets passenger name
 void setName(string _name);
+//returns passenger name
 string getName();
+//adds a flight to the passenger's list of flights
 void addFlight (Flight* _flight);
+//removes a flight from the passenger's list of flights
 void removeFlight(Flight* _flight);
+//returns the passenger's list of flights
 vector<Flight*> getFlights();
+
 };
