bf08b3b71c01fa75411eea95307a35f8c49c27c0
aciucanu <aciucanu@localhost>
2013-11-22 11:18:58 +0000

    main file

---
+++ main.cpp
@@ -0,0 +1,44 @@
+void main()
+{
+	//Create model - populate with flights (sorted by code and date)
+	//Create view
+	//View has pointer to model
+	//Create Controller with pointer to model and view
+	//or 
+	//Create Controller and instantiate model and views in Controller constructor
+	//Deallocate memory in destructor
+	
+
+	Model* FRSModel = Model::getModel();
+	AbstractView* FRSView = new CLIView();
+	Controller* FRSController = new Controller(FRSModel, FRSView);
+	FRSView.setController(FRSController);
+
+
+	//Reservation
+	//Create/Select a passenger
+	//Choose flight and class
+	//Add passenger to flight and flight to passenger
+
+
+	//Cancellation
+	//Select a passenger
+	//Select flight from passenger's list
+	//Remove passenger from flight and flight from passenger
+	
+
+	//Passenger Inquiry
+	//Select a passenger
+	//Print out flights related to that passenger
+	
+
+	//Flight Inquiry
+	//Select a flight
+	//Print out passengers related to that flight
+
+
+//Extras?
+	//Remove a passenger from model
+	//Remove a flight from model
+
+}
