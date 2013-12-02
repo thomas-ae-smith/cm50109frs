98903f9914e635d4c27ee322a4382ff4e5fcffce
aciucanu <aciucanu@localhost>
2013-12-02 19:55:32 +0000

    Extras for testing, makefile works now.

--- testing/controller_harness.cpp
+++ testing/controller_harness.cpp
@@ -1,13 +1,27 @@
 #include "../Controller.h"
+#include "../CLIView.h"
 #include <iostream>
+
+
+void test(Controller *_controller)
+{
+//Test each method in thye controller class
+
+ 
+}
 
 
 //Testing the flight class and its methods
 
 int main()
 {
+AbstractView* view = new CLIView();
+Model* model = Model::getModel();
 
+Controller* controller = new Controller(model, view);
 cout<<"Hello Controller harness!";
+
+test(controller);
 
 return 0;
 }
