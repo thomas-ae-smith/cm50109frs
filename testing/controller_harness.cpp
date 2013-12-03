17432cacc0878e78156ae3f1151a070d456e575b
aciucanu <aciucanu@localhost>
2013-12-03 10:23:12 +0000

    controller testing update 1

--- testing/controller_harness.cpp
+++ testing/controller_harness.cpp
@@ -3,11 +3,18 @@
 #include <iostream>
 
 
+void test_makeReservation(Controller *_controller)
+{
+  cout<<"Controller, I am in the test_makeReservation method!"<<endl;
+  _controller->makeReservation("John Atkinson", "AA321", "First");
+
+}
+
 void test(Controller *_controller)
 {
 //Test each method in thye controller class
-
- 
+ cout<<"Controller, I am in the test method!"<<endl;
+ test_makeReservation(_controller);
 }
 
 
@@ -19,7 +26,6 @@ AbstractView* view = new CLIView();
 Model* model = Model::getModel();
 
 Controller* controller = new Controller(model, view);
-cout<<"Hello Controller harness!";
 
 test(controller);
 
