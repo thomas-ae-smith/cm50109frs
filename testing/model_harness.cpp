cfa2626b4f56a1c49ecd6d43012445373d97929e
Yaping Lang <yl2025@bath.ac.uk>
2013-12-05 17:19:16 +0000

    Harnessed with daterange thing.

--- testing/model_harness.cpp
+++ testing/model_harness.cpp
@@ -7,12 +7,17 @@
 
 int main()
 {
+    Model();
+    map<string,Passenger*> m_passengers;
+    map<string,Flight*> m_flightByCode;
+    multimap<string,Flight*> m_flightByDate;
+    
     
     //Construct a flight
-    Flight* flight1 = new Flight("Code1", "0900", "25102012", 250);
-    Flight* flight2 = new Flight("Code2", "1000","25112013",260);
-    Flight* flight3 = new Flight("Code3","1100","25112013",270);
-    Flight* flight4 = new Flight("Code4","1200","25112013",280);
+    Flight* flight1 = new Flight("Code1", "0900", "25102012", 10);
+    Flight* flight2 = new Flight("Code2", "1000","25112013",10);
+    Flight* flight3 = new Flight("Code3","1100","25112013",10);
+    Flight* flight4 = new Flight("Code4","1200","25112013",10);
     
     
     //Construct a passenger
@@ -22,26 +27,12 @@ int main()
     my_model->addFlight(flight3);
     my_model->addFlight(flight4);
     
-    my_model->addPassenger("Emily");
-    my_model->addPassenger("LILY");
-    my_model->addPassenger("TOM");
-    my_model->addPassenger("Anamaria");
-    
-    
-    
-    //Returns a passenger by name
-    Passenger* passenger1 = my_model->getPassengerByName("Emily");
-    cout<< passenger1->getName()<<endl;
-    Passenger* passenger2 =my_model->getPassengerByName("Julian");
-    if (passenger2!=nullptr) {
-        cout<<"This is a bad pointer to somewhere.\n"<<endl;
+    for(map<string,Flight*>::iterator it=m_flightByCode.begin();it!=m_flightByCode.end();++it){
+        cout<< "checking the flight by code database."<<endl;
+        cout<< it->first << (it->second)->getDate() << (it->second)->getCode() <<endl;
+        cout<< "Succeed!. "<< endl;
     }
-    else
-        cout<<"This is NUll!!!\n"<<endl;
     
-    //Returns a flight by its code
-    Flight* cflight=my_model->getFlightByCode("Code1");
-    cout<<cflight->getDate()<<"  "<<cflight->getCode()<<endl;
     
     //Get a pointer to a vector of flights on the same date
     vector<Flight*>* t_dateflight =my_model->getFlightsByDate("25122013");
@@ -52,33 +43,46 @@ int main()
     }
     
     
+    my_model->addPassenger("Emily");
+    my_model->addPassenger("LILY");
+    my_model->addPassenger("TOM");
+    my_model->addPassenger("Anamaria");
+    
+    
+    
     //Adds a new reservation to the data base
-    void addReservation(string _name, string _code, Flight::SeatClass _class);
-    my_model->addReservation("Alice", "Code1", First);
+    // void addReservation(string _name, string _code, Flight::SeatClass _class);
+    my_model->addReservation("Emily", "Code1", Flight::First);
+    my_model->addReservation("Tom", "Code1", Flight::First);
+    
+    //Returns a flight by its code
+    Flight* cflight=my_model->getFlightByCode("Code1");
+    cout<<cflight->getDate()<<"  "<<cflight->getCode()<<endl;
+    cout<<cflight->getSeatAvailability(Flight::First)<<endl;
+    cout<<cflight->Booked<<endl;
+    //Returns a passenger by name
+    Passenger* passenger1 = my_model->getPassengerByName("Emily");
+    cout<< passenger1->getName()<<endl;
+    Passenger* passenger2 =my_model->getPassengerByName("Julian");
+    if (passenger2!=nullptr) {
+        cout<<"This is a bad pointer to somewhere.\n"<<endl;
+    }
+    else
+        cout<<"No passenger in this name!!!\n"<<endl;
     
     
-    //Adds a flight to the database
+    
+    
+    
+    
     
     //Given a string, returns the class it corresponds to
-    Flight::SeatClass getClass(string _class);
     
     //Makes a passenger inquiry and returns the flights a passenger is currently on
     vector<Flight*>* t_passengerinquiry=my_model->makePassengerInquiry("Emily");
+    for (vector<Flight*>::iterator it =(*t_passengerinquiry).begin(); it!=(*t_passengerinquiry).end(); ++it)
+    {
+        cout<< (*it)->getCode()<<endl;
+    }
     
-    //Make a flight inquiry and return a pointer to a pair of iterators, delimiting a list of booked/waiting passengers
-    pair<vector<Passenger*>::iterator, vector<Passenger*>::iterator>*  t_fligthinquiry=my_model->makeFlightInquiry("Code1",First, booked);
-    
-	//private://
-    //Map of all passengers, sorted by name
-    map<string,Passenger*> m_passengers;
-    
-    //Map of all flights, sorted by code
-    map<string,Flight*> m_flightByCode;
-    
-    //Multimap of all flights, sorted by date
-    multimap<string,Flight*> m_flightByDate;
-    
-    
-    //Private constructor and destructor for the singleton pattern
-    Model();
 }
